eada3486f44408ba35c997a99531c0be49061864
hyungchunKim <131167225+hyungchunKim@users.noreply.github.com>
2023-08-22 04:26:10 +0900

    [Feat] 회원가입 기능구현
    회원가입에 대한 전반적인 구현 완료
    
    유효성 검사 후 나타나는 메세지 상태에 따라 색깔 다르게 나타나도록 하는 부분 보완필요
    
    로그인 하기 누르면 로그인 모달 띄우는 부분 구현 필요
    
    버튼 눌렀을 때 에러메세지 부분 구현 필요

---
+++ vite-project/src/Pages/Signup.tsx
@@ -0,0 +1,305 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+
+const Signup = () => {
+  
+  interface form {
+    email: null | string;
+    password: null | string;
+    username: null | string;
+    birth: null | string;
+  }
+
+  interface isValidForm {
+    isValidEmail: boolean;
+    isValidPassword: boolean;
+    isValidUsername: boolean;
+    isValidBirth: boolean;
+  }
+
+  interface formMessage {
+    formEmailMessage: null | string;
+    formPasswordMessage: null | string;
+    formUsernameMessage: null | string;
+    formBirthMessage: null | string;
+  }
+  
+  const debounce = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
+    let timeout: ReturnType<typeof setTimeout>;
+
+    return (...args: Parameters<T>): ReturnType<T> => {
+      let result: any;
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        result = fn(...args);
+      }, delay);
+      return result;
+    };
+  };
+
+  const [form, setForm] = useState<form>({
+    email: "",
+    password: "",
+    username: "",
+    birth: ""
+  });
+
+  const [isValid, setIsValid] = useState<isValidForm>({
+    isValidEmail: false,
+    isValidPassword: false,
+    isValidUsername: false,
+    isValidBirth: false
+  });
+
+  const [message, setMessage] = useState<formMessage>({
+    formEmailMessage: "",
+    formPasswordMessage: "",
+    formUsernameMessage: "",
+    formBirthMessage: ""
+  });
+
+  const emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegexp = /^(?=.*[a-z])(?=.*\d)[a-z\d]{4,14}$/;
+  const usernameRegexp = /^(?!.*[ㄱ-ㅎㅏ-ㅣ])(?!^[ㄱ-ㅎㅏ-ㅣ]*$)(?!.*[aeiouAEIOU])[ㄱ-ㅎㅏ-ㅣ가-힣]{2,5}$/;
+  const birthRegexp = /^(?!0000)(?!00)(?:\d{4})-(?:(?:0[1-9])|(?:1[0-2]))-(?:(?:0[1-9])|(?:[1-2][0-9])|(?:3[0-1]))$/;
+  
+  const isValidEmail = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+    
+    const currentEmail = e.target.value;
+    setForm({...form, email: currentEmail});
+    console.log(currentEmail);
+
+    if(!emailRegexp.test(currentEmail)) {
+      setIsValid({...isValid, isValidEmail: false});
+      setMessage({...message, formEmailMessage: "이메일 형식에 맞게 작성해주세요."});
+    } else {
+      setIsValid({...isValid, isValidEmail: true});
+      setMessage({...message, formEmailMessage: "유효한 이메일 형식입니다."});
+    }
+  }, 800);
+
+  const isValidPassword = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+    
+    const currentPassword = e.target.value;
+    setForm({...form, password: currentPassword});
+    console.log(currentPassword);
+
+    if(!passwordRegexp.test(currentPassword)) {
+      setIsValid({...isValid, isValidPassword: false});
+      setMessage({...message, formPasswordMessage: "영문 소문자, 숫자 포함 4자 이상 14자 이하로 입력해주세요."});
+    } else {
+      setIsValid({...isValid, isValidPassword: true});
+      setMessage({...message, formPasswordMessage: "유효한 비밀번호 형식입니다."});
+    }
+  }, 800);
+
+  const isValidUsername = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+    
+    const currentUsername = e.target.value;
+    setForm({...form, username: currentUsername});
+    console.log(currentUsername);
+
+    if(!usernameRegexp.test(currentUsername)) {
+      setIsValid({...isValid, isValidUsername: false});
+      setMessage({...message, formUsernameMessage: "2자 이상 5자 이하의 한글 성명을 입력해주세요."});
+    } else {
+      setIsValid({...isValid, isValidUsername: true});
+      setMessage({...message, formUsernameMessage: "유효한 한글 성명 형식입니다."});
+    }
+  }, 800);
+
+  const isValidBirth = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+    
+    const currentBirth = e.target.value;
+    setForm({...form, birth: currentBirth});
+    console.log(currentBirth);
+
+    if(!birthRegexp.test(currentBirth)) {
+      setIsValid({...isValid, isValidBirth: false});
+      setMessage({...message, formBirthMessage: `"yyyy-mm-dd"를 만족하는 8자의 생년월일을 입력해주세요. 중간에 "-"를 반드시 입력해주세요.`});
+    } else {
+      setIsValid({...isValid, isValidBirth: true});
+      setMessage({...message, formBirthMessage: "유효한 생년월일 형식입니다."});
+    }
+  }, 800);
+
+  const onSubmit = async() => {
+    return await axios
+    .post('http://3.34.48.249:8080/api/user/signup', {
+      email: form.email,
+      password: form.password,
+      username: form.username,
+      birth: form.birth
+    })
+    .then(function (res) {
+      console.log(res);
+    })
+  }
+
+  return (
+    <ModalContainer>
+      <ModalBackdrop>
+        <ModalView>
+          <ModalHeader>
+            <ModalHeaderDiv>
+              <CloseButton>
+                <StyledFontAwesomeIcon icon={faX}/>
+              </CloseButton>
+            </ModalHeaderDiv>
+            <SignupHeader>회원 가입</SignupHeader>
+            <ModalHeaderDiv></ModalHeaderDiv>
+          </ModalHeader>
+          <ModalForm>
+            <ModalH1>내집어때</ModalH1>
+            <ModalInput placeholder='이메일' type='email' onChange={isValidEmail}/>
+            <ModalInputMessage>{message.formEmailMessage}</ModalInputMessage>
+            <ModalInput placeholder='비밀번호' type='password' onChange={isValidPassword}/>
+            <ModalInputMessage>{message.formPasswordMessage}</ModalInputMessage>
+            <ModalInput placeholder='이름' type='name' onChange={isValidUsername}/>
+            <ModalInputMessage>{message.formUsernameMessage}</ModalInputMessage>
+            <ModalInput placeholder='생년월일 8자' type='birth' onChange={isValidBirth}/>
+            <ModalInputMessage>{message.formBirthMessage}</ModalInputMessage>
+            <ModalSubmit 
+              disabled={isValid.isValidEmail && isValid.isValidPassword && isValid.isValidUsername && isValid.isValidBirth ? false : true}
+              onClick={onSubmit}
+            >가입 완료
+            </ModalSubmit>
+            <ModalSigninPath>로그인 하기</ModalSigninPath>
+          </ModalForm>
+        </ModalView>
+      </ModalBackdrop>
+    </ModalContainer>
+  );
+};
+
+const ModalContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+`;
+
+const ModalBackdrop = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: 9999;
+`
+
+const ModalView = styled.div`
+  width: 500px;   
+  height: 700px;  
+  margin: auto;
+  padding: 0px 0px 24px;   
+  border-radius: 20px;
+  background-color: white;
+  z-index: 10000;
+`;
+
+const ModalHeader = styled.div`
+  height: 64px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px #ebebeb solid;
+`;
+
+const ModalHeaderDiv = styled.div`
+  margin: zero;
+  flex: 1;
+`;
+
+const CloseButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  margin-left: 24px;
+`;
+
+const SignupHeader = styled(ModalHeaderDiv)`
+  text-align: center;
+  font-weight: 700;
+`;
+
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  font-size: 16px;
+`;
+
+const ModalForm = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  padding: 0 96px;
+  align-items: center;
+
+  & > h1 {
+    align-items: left;
+  }
+`;
+
+const ModalInput = styled.input.attrs({ required: true })`
+  display: block;
+  width: 406px;
+  height: 50px;
+  font-size: 20px;
+  border-radius: 8px;
+  border: 1px rgba(0, 0, 0, 0.3) solid;
+  text-indent: 12px;
+  
+  &:focus {
+    border: 2px green solid;
+    outline: none;
+  }
+`;
+
+const ModalInputMessage = styled.p`
+  font-size: 10px;
+  height: 12px;
+  margin: 6px 0;
+  font-family: 'Single Day', cursive;
+  width: 380px;
+`
+
+const ModalH1 = styled.h1`
+  text-align: center;
+  font-family: 'Single Day', cursive;
+  width: 150px;
+  height: 50px;
+  line-height: 50px;
+  background-color: #fc335a;
+  color: white;
+  border-radius: 20px 100px 20px 100px;
+  margin-bottom: 40px;
+`;
+
+const ModalSubmit = styled.button`
+  display: block;
+  width: 408px;
+  height: 50px;
+  background-color: #fc335a;
+  text-align: center;
+  margin-top: 30px;
+  border: none;
+  border-radius: 8px;
+  color: white;
+  font-size: 20px;
+  pointer: cursor;
+`;
+
+const ModalSigninPath = styled.div`
+  text-decoration: none;
+  color: #fc335a;
+  margin-top: 60px;
+  pointer: cursor;
+` 
+
+export default Signup;
