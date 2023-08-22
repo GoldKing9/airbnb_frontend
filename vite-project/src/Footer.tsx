b59dbd28d498f6022fb39dcaeffc0ba4cb91bc1f
GHeeJeon <jhjeon_99@naver.com>
2023-08-22 18:36:24 +0900

    [Feat] update footer

--- vite-project/src/Footer.tsx
+++ vite-project/src/Footer.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterContainer = styled.div`
-  padding: 20px;
-  background-color: #f7f7f7;
+  padding: 10px;
+  background-color: #ffffff;
   width: 100%; // 너비를 100%로 설정
+  border-top: 1px solid #EBEBEB;
 `;
 
 const Info = styled.div`
   font-size: 14px;
   margin-bottom: 10px;
+  color: #222222;
 `;
 
 const Footer: React.FC = () => {
     return (
         <FooterContainer>
-            <Info>© 2021 Airbnb, Inc. All rights reserved</Info>
+            <Info>© 2023 내집어때, Inc. All rights reserved</Info>
             <div>
                 <span>Privacy</span> · <span>Terms</span> · <span>Sitemap</span>
             </div>
