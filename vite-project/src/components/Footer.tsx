55b906ff258062f0a940fd99a4abd92f6df75050
GHeeJeon <jhjeon_99@naver.com>
2023-08-30 05:01:53 +0900

    [Feat] update layout

--- vite-project/src/components/Footer.tsx
+++ vite-project/src/components/Footer.tsx
@@ -5,24 +5,29 @@ const Footer: React.FC = () => {
     return (
         <FooterContainer>
             <Info>© 2023 내집어때, Inc. All rights reserved</Info>
-            <div>
+            <h3>
                 <span>Privacy</span> · <span>Terms</span> · <span>Sitemap</span>
-            </div>
+            </h3>
         </FooterContainer>
     );
 };
 
 const FooterContainer = styled.div`
-  padding: 10px;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
   background-color: #ffffff;
-  width: 100%; // 너비를 100%로 설정
   border-top: 1px solid #EBEBEB;
 `;
 
 const Info = styled.div`
   font-size: 14px;
-  margin-bottom: 10px;
   color: #222222;
+  margin-top: 10px;
 `;
 
 export default Footer;
--- vite-project/src/pages/Show.tsx
+++ vite-project/src/pages/Show.tsx
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
-const App: React.FC = () => {
+type Accommodation = {
+    accommodationId: number;
+    mainAddress: string;
+    price: number;
+    ratingAvg: number;
+    images: { acmdImageUrl: string | null }[];
+};
+
+const Show: React.FC = () => {
+    const [results, setResults] = useState<Accommodation[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://3.39.233.168:8080/api/accommodation/search');
+                setResults(response.data.results);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
-        <h1>Show component</h1>
+        <div>
+            <Container>
+                {results.map(accommodation => (
+                    <Card key={accommodation.accommodationId}>
+                        {accommodation.images[0]?.acmdImageUrl && (
+                            <Image src={accommodation.images[0].acmdImageUrl} alt="Accommodation" />
+                        )}
+                        <h4>{accommodation.mainAddress}</h4>
+                        <p>Price: ₩{accommodation.price}</p>
+                        <p>Average Rating: {accommodation.ratingAvg}</p>
+                    </Card>
+                ))}
+            </Container>
+        </div>
     );
 };
 
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); // 4개의 열 생성
+  gap: 16px; // 각 항목 사이의 간격
+  padding: 16px;
+  overflow-y: auto; // 세로 스크롤 활성화
+  max-height: 100vh; // 뷰포트 높이를 최대로 설정
+`;
 
-export default App;
+const Card = styled.div`
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  margin: 8px;
+  padding: 16px;
+  text-align: start;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    max-height: 200px;
+    object-fit: cover;
+    border-radius: 8px;
+`;
+
+
+export default Show;
